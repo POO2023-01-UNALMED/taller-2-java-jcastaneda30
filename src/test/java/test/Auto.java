91141d3c991cc1149fcec1bde1ff021cf8f54c95
jcastaneda30 <119982731+jcastaneda30@users.noreply.github.com>
2023-02-19 21:18:28 -0500

    Add files via upload

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,41 @@
+package test;
+
+public class Auto {
+	public String modelo;
+	public int precio;
+	public Asiento asientos[];
+	public String marca;
+	public Motor motor;
+	public int registro;
+	public static int cantidadCreados;
+	boolean pirata = true;
+	String mensaje;
+	public int cantidadAsientos() {
+		int cantidad = 0;
+		for (int i=0; i<asientos.length;i++) {
+			if (asientos[i]!=null) {
+				cantidad++;
+			}
+		}
+		return cantidad;
+	}
+	public String verificarIntegridad(){
+		if (motor.registro==registro) {
+			for (int i=0; i<asientos.length;i++) {
+			if (asientos[i]!=null) {
+				if(registro!=asientos[i].registro) {
+					pirata = false; 
+					break;
+				}
+			}
+		}
+		if (pirata==false) {
+			mensaje  = "Las piezas no son originales";
+		}else {
+			mensaje = "Auto original";
+		}
+		
+	}
+		return mensaje;
+	}
+}
